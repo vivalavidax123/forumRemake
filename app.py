26208465e07dfe05059a66eb0b5aaf43c9d5afd8
Zeming Xia <zemxia@student.unimelb.edu.au>
2025-04-25 21:31:10 +1000

    显示注册用户以及注册跳转页面

--- app.py
+++ app.py
@@ -20,6 +20,10 @@ def index():
 def login():
     return render_template('login.html')
 
+@app.route('/register')
+def register():
+    return render_template('register.html')
+
 @app.route('/write')
 def write():
     return render_template('write.html')
